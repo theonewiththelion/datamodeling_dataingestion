e62749128f1b2b061d48eca765bdb731d5c3b055
theonewiththelion <jonathan.dejesus.azor@gmail.com>
2022-09-15 09:35:23 -0500

    Starting project for Data Ingestion

---
+++ animal_DataIngestion.py
@@ -0,0 +1,21 @@
+"""
+Extract value from:
+3 fields through API -> store in a df
+3 fields through -> CSV
+3 fields through -> JSON
+
+
+Transform
+Separate dates
+drop values
+etc.
+
+Load
+Warehouse with data model
+data lake
+
+Visualization
+PowerBI (Need Windows, will be leveraging Github to move code)
+
+
+"""
