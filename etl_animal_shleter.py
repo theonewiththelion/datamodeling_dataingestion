c1090eb646859652a76a6096cac7c5c4710ebd15
theonewiththelion <jonathan.dejesus.azor@gmail.com>
2022-09-15 10:31:37 -0500

    Starting extraction

---
+++ etl_animal_shleter.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import pandas_gbq
+import requests
+import json
+
+def api_extraction():
+
+    animal_shelter_api = requests.get("https://data.austintexas.gov/resource/9t4d-g238.csv?$limit=10")
+    #animal_shelter_api = requests.get("https://data.austintexas.gov/resource/9t4d-g238.json?$limit=10")
+    #Save API into a variable
+    animal_shelter_table = animal_shelter_api.text
+    print(animal_shelter_table)
+
+api_extraction()
